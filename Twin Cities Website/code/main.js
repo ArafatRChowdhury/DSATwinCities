b38852b473275be9a9d7d89f310170b7b1784d1a
ArafatRChowdhury <148768784+ArafatRChowdhury@users.noreply.github.com>
2025-02-18 16:40:18 +0000

    Update main.js

--- Twin Cities Website/code/main.js
+++ Twin Cities Website/code/main.js
@@ -13,25 +13,91 @@ import { fromLonLat } from 'ol/proj';
 import { defaults as defaultInteractions } from 'ol/interaction';
 
 //constants for the co ordinates of the cities and places of interest
-const liverpoolCenter = fromLonLat([-2.983333, 53.400002]);
-const rioDeJaneiroCenter = fromLonLat([-43.196388, -22.908333]);
+const liverpoolCenter = fromLonLat([-2.99, 53.40050]);
+const liverpoolMountain = fromLonLat([-2.994611, 53.401065]);
+const beatlesStatue = fromLonLat([-2.996435, 53.404513]);
+const liverpoolMuseum = fromLonLat([-2.995577, 53.403483]);
+const theBluecoat = fromLonLat([-2.983787, 53.404235]);
+const maldronHotel = fromLonLat([-2.986259, 53.400578]);
+const chavassePark = fromLonLat([-2.988920, 53.404077]);
+
+
+const rioDeJaneiroCenter = fromLonLat([-43.196388, -22.958333]);
+const maracanaStadium = fromLonLat([-43.230139, -22.912062]);
+const museoDeAmanha = fromLonLat([-43.179587, -22.894384]);
+const christTheRedeemer = fromLonLat([-43.209631, -22.951406]);
+const sugarloafMountain = fromLonLat([-43.154546, -22.949299]);
+const terraBrasilis = fromLonLat([-43.164847, -22.956182]);
+const copacabanaPalace = fromLonLat([-43.178958, -22.967272]);
 
 
 //constants for the markers themselves - new features are created which sets a point at the co ordinates
-const liverpool = new Feature({
-  geometry: new Point(liverpoolCenter),
-  name: 'Liverpool',
+
+const liverpoolMuseumMarker = new Feature({
+  geometry: new Point(liverpoolMuseum),
+  name: 'Liverpool Museum',
 });
 
-const rioDeJaneiro = new Feature({
-  geometry: new Point(rioDeJaneiroCenter),
-  name: 'Rio de Janeiro',
+const liverpoolMountainMarker = new Feature({
+  geometry: new Point(liverpoolMountain),
+  name: 'Liverpool Mountain',
+});
+
+const beatlesStatueMarker = new Feature({
+  geometry: new Point(beatlesStatue),
+  name: 'Beatles Statue',
+});
+
+const theBluecoatMarker = new Feature({
+  geometry: new Point(theBluecoat),
+  name: 'The Bluecoat',
+});
+
+const maldronHotelMarker = new Feature({
+  geometry: new Point(maldronHotel),
+  name: 'Maldron Hotel',
+});
+
+const chavasseParkMarker = new Feature({
+  geometry: new Point(chavassePark),
+  name: 'Chavasse Park',
+});
+
+
+const maracanaStadiumMarker = new Feature({
+  geometry: new Point(maracanaStadium),
+  name: 'maracanaStadium',
+});
+
+const museoDeAmanhaMarker = new Feature({
+  geometry: new Point(museoDeAmanha),
+  name: 'Museo de Amanha',
+});
+
+const christTheRedeemerMarker = new Feature({
+  geometry: new Point(christTheRedeemer),
+  name: 'Christ the Redeemer',
+});
+
+const sugarloafMountainMarker = new Feature({
+  geometry: new Point(sugarloafMountain),
+  name: 'Sugarloaf Mountain',
+});
+
+const terraBrasilisMarker = new Feature({
+  geometry: new Point(terraBrasilis),
+  name: 'Terra Brasilis',
+});
+
+const copacabanaPalaceMarker = new Feature({
+  geometry: new Point(copacabanaPalace),
+  name: 'Copacabana Palace',
 });
 
 //how the markers look
 const iconStyle = new Style({
   image: new Icon({
-    anchor: [0.5, 46],
+    anchor: [0.5, 1],
     anchorXUnits: 'fraction',
     anchorYUnits: 'pixels',
     src: 'icons/marker.png',
@@ -39,16 +105,35 @@ const iconStyle = new Style({
   }),
 });
 
-liverpool.setStyle(iconStyle);
-rioDeJaneiro.setStyle(iconStyle);
+
+liverpoolMountainMarker.setStyle(iconStyle);
+beatlesStatueMarker.setStyle(iconStyle);
+liverpoolMuseumMarker.setStyle(iconStyle);
+theBluecoatMarker.setStyle(iconStyle);
+maldronHotelMarker.setStyle(iconStyle);
+chavasseParkMarker.setStyle(iconStyle);
+
+maracanaStadiumMarker.setStyle(iconStyle);
+museoDeAmanhaMarker.setStyle(iconStyle);
+christTheRedeemerMarker.setStyle(iconStyle);
+sugarloafMountainMarker.setStyle(iconStyle);
+terraBrasilisMarker.setStyle(iconStyle);
+copacabanaPalaceMarker.setStyle(iconStyle);
 
 const vectorSource = new VectorSource({
-  features: [liverpool, rioDeJaneiro],
+  features: [liverpoolMountainMarker, beatlesStatueMarker, liverpoolMuseumMarker, theBluecoatMarker, maldronHotelMarker, chavasseParkMarker,
+  maracanaStadiumMarker, museoDeAmanhaMarker, christTheRedeemerMarker, sugarloafMountainMarker, terraBrasilisMarker, copacabanaPalaceMarker],
 });
 
 const vectorLayer = new VectorLayer({
   source: vectorSource,
 });
+
+const view = new View({
+  center: [0, 0],
+  zoom: 1,
+});
+//view is around 15 for liverpool, 12.5 for rio de janeiro
 
 const map = new Map({
   layers: [
@@ -58,12 +143,7 @@ const map = new Map({
     vectorLayer,
     ],
   target: document.getElementById('map'),
-  view: new View({
-    center: liverpoolCenter,
-    //minZoom: 15,
-    //maxZoom: 15,
-    zoom: 15,
-  }),
+  view: view,
   interactions: defaultInteractions({
     dragPan: false,
     doubleClickZoom: false,
@@ -74,6 +154,28 @@ const map = new Map({
     pointer: false
   })
 });
+
+const centerOnLiverpool = document.getElementById('centerOnLiverpool');
+centerOnLiverpool.addEventListener(
+  'click',
+  function() {
+    const size = map.getSize();
+    view.setCenter(liverpoolCenter),
+    view.setZoom(15)
+  },
+  false,
+);
+
+const centerOnRioDeJaneiro = document.getElementById('centerOnRioDeJaneiro');
+centerOnRioDeJaneiro.addEventListener(
+  'click',
+  function() {
+    const size = map.getSize();
+    view.setCenter(rioDeJaneiroCenter),
+    view.setZoom(12.5)
+  },
+  false,
+);
 
 const element = document.getElementById('popup');
 
