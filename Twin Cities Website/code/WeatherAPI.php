dfb19a463e15d3a1c837e5b115cdb4a4d59ea76b
Jaimepv1 <148768760+Jaimepv1@users.noreply.github.com>
2025-03-11 10:02:45 +0000

    Update WeatherAPI.php

--- Twin Cities Website/code/WeatherAPI.php
+++ Twin Cities Website/code/WeatherAPI.php
@@ -2,7 +2,7 @@
 
 <title>Liverpool & Rio de Janeiro Weather</title>
     <body style="background-color:lightblue;">
-        <h2 style="padding-left:310px;">Weather Report</h2> 
+        <h2 style="padding-left:310px; padding-top:10px;">Weather Report</h2> 
         <button style="width: 120px; height: 30px; border-radius:12px;">Go back to menu</button>
 <?php
 
@@ -27,21 +27,20 @@ $liverpoolforecast = simplexml_load_file($liverpoolforecasturl);
 $janeiroforecast = simplexml_load_file($janeiroforecasturl);
 
 //Function to get weather data and display it in order of what was requested
-function currentweatherinCity($weather, $city) {
-    echo "<div style='border:10px solid #000000; padding: 10px; margin: 10px;'>";
-    echo "<h2><u>Current Weather in $city on " . date('D dS F Y : H:i:s') . "</u></h2>";//Displays a header with the name of the city and the time
-    echo "<p><u>Condition:</u> " . ucfirst($weather->weather['value'] . "</p>");//ucfirst makes the first character of the sentence a capital letter
-    echo "<p><u>Temperature:</u> " . $weather->temperature['value'] . "°C"."</p>"; //Displays the weather in Celsius
-    echo "<p><u>Humidity:</u> " . $weather->humidity['value'] . "%"."</p>";//Displays the humidity
-    echo "<p><u>Sunrise:</u> " . date('G:i:s', strtotime($weather->city->sun['rise'])) . "</p>";//Displays the sunrise time
-    echo "<p><u>Sunset:</u> " . date('G:i:s', strtotime($weather->city->sun['set'])) . "</p>";//Displayes the sunset time
-    echo "</div>";
+function currentweatherinCity($weather,$city){
+    echo "<div style='border:10px solid #000000; padding:10px; margin:10px;'>";
+    echo "<h2><u> Current Weather in $city on ". date('D dS F Y : H:i:s')."</u></h2>";//Displays a header with the name of the city and the time
+    echo "<p><u>Temperature :</u> " . $weather->temperature['value'] . "°C"."</p>"; //Displays the weather in Celsius
+    echo "<p><u>Condition :</u> " . ucfirst($weather->weather['value'] . "</p>");//ucfirst makes the first character of the sentence a capital letter
+    echo "<p><u>Sunrise :</u> " . date('G:i:s', strtotime($weather->city->sun['rise'])) . "</p>";//Displays the sunrise time
+    echo "<p><u>Sunset :</u> " . date('G:i:s', strtotime($weather->city->sun['set'])) . "</p>";//Displayes the sunset time
+    echo "<p><u>Humidity :</u> " . $weather->humidity['value'] . "%"."</p>";//Displays the humidity
 }
 
 // Function to get forecast data and display it 
 function forecastinCity($forecast, $city) {
-    echo "<div style='border:10px solid #000000; padding: 5px; margin: 10px;'>";
-    echo "<h2><u>5-Day Forecast for $city</u></h2>";//Displays a header with the name of the city
+    echo "<div style='border:1px solid #000000; padding: 6px; margin: 0px;'>";
+    echo "<h1><u>5-Day Forecast for $city</u></h1>";//Displays a header with the name of the city
  
     $currentDate = null;
     //Loop to only display 5 days of forecast
@@ -50,15 +49,12 @@ function forecastinCity($forecast, $city) {
         $date = date('Y-m-d', $forecastTime);
         $hour = date('H', $forecastTime);
         if ($hour == "12") {
-            if ($currentDate !== $date) {
-                $currentDate = $date;
 
                 echo "<h3>" ."<u>". date('D, d M Y', $forecastTime) ."</u>". "</h3>";//Displays a header with the date for a 5 day forecast
-                echo "<p><u>Condition:</u> " . ucfirst($time->symbol['name'] . "</p>");//Displays the condition of the weather
-                echo "<p><u>Temperature:</u> " . $time->temperature['value'] . "°C"."</p>";//Displays the temperature in Celsius
-                echo "<p><u>Humidity:</u> " . $time->humidity['value'] . "%</p>";//Displays the humidity
+                echo "<p><u>Temperature :</u> " . $time->temperature['value'] . "°C"."</p>";//Displays the temperature in Celsius
+                echo "<p><u>Condition :</u> " . ucfirst($time->symbol['name'] . "</p>");//Displays the condition of the weather
+                echo "<p><u>Humidity :</u> " . $time->humidity['value'] . "%</p>";//Displays the humidity
                 echo "<br>";
-            }
         }
     }
     echo "</div>";
@@ -85,4 +81,3 @@ echo "</div>";
    </style>
  </body>
 </html>
-
